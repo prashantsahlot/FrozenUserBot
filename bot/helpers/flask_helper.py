ae00efbf96725894c261fab98d3dde08147a05ce
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-10-19 12:30:14 +0530

    Update flask_helper.py

--- bot/helpers/flask_helper.py
+++ bot/helpers/flask_helper.py
@@ -1,1 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app_flask = Flask("")
+
+@app_flask.route("/")
+def home():
+    return "Bot is running!"
+
+def run():
+    app_flask.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
