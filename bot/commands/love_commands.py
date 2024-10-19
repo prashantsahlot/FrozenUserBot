d12b08d0366653df4c03166217b8933ddb896232
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-10-19 12:31:30 +0530

    Update love_commands.py

--- bot/commands/love_commands.py
+++ bot/commands/love_commands.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import asyncio
+
+@app.on_message(filters.command(["sayang", "lover"], ".") & filters.me)
+async def zeyenk(client: Client, message: Message):
+    e = await message.reply("I LOVE YOUUU 💕")
+    await asyncio.sleep(0.2)
+    await e.edit("💝💘💓💗")
+    await asyncio.sleep(0.2)
+    await e.edit("💞💕💗💘")
+    await asyncio.sleep(0.2)
+    await e.edit("💘💞💕💗")
+    await asyncio.sleep(0.2)
+    await e.edit("LOVE YOU 💝💖💘")
+    await asyncio.sleep(0.2)
+    await e.edit("LOVE YOU FOREVER 💕")
+    await asyncio.sleep(0.2)
+    await e.edit("💘 You're my star 💖")
 
